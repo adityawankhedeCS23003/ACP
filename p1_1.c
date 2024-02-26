6bdf33c904b04ab959af794aec7c59fabc5100ea
Aspect <157013297+adityawankhedeCS23003@users.noreply.github.com>
2024-02-26 19:48:41 +0530

    Update p1_1.c

--- p1_1.c
+++ p1_1.c
@@ -9,11 +9,8 @@ int main()
     for ( i = 0; i < n; i++)
     {
         scanf("%d",&arr[i]);
-    }
-    for ( i = 0; i < n; i++)
-    {
         sum+=arr[i];
     }
-    printf("Sum %d",sum);
+    printf("Sum is %d",sum);
     
 }
