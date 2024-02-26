77e41e34f27a66c7bd764136c8599a06f451be1a
Aspect <157013297+adityawankhedeCS23003@users.noreply.github.com>
2024-02-26 19:59:51 +0530

    Create p2_2.c

---
+++ p2_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *ptr,i,n;
+    float avg,sum;
+    printf("Enter the no of elements for array ");
+    scanf("%d",&n);
+    ptr=(int*)malloc(n*sizeof(int)); //dynamic memory allocation
+    printf("Enter the elements of array\n");
+    for (int i = 0; i < n; i++)
+    {
+       scanf("%d", &ptr[i]);
+       sum+=ptr[i];
+    }
+    printf("Elements of array\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", ptr[i]);
+    }
+    avg=sum/n;
+    printf("\nAverage of elements in the array is %.2f\n",avg);
+    free(ptr);
+    return 0;
+}
