d2c13a42cf9210b601eb1aa4d48e3d983020043d
Aspect <157013297+adityawankhedeCS23003@users.noreply.github.com>
2024-02-27 23:35:34 +0530

    Update p1_8.c

--- p1_8.c
+++ p1_8.c
@@ -39,21 +39,7 @@ void main()
                 printf("%d ",mat1[i][j]);
             }
             printf("\n");            
-    }
-
-    // for (int i = 0; i < 2; i++)
-    // {
-    //         for (int j = 0; j<2; j++)
-    //         {
-    //             mat3[i][j]=0;            
-    //             for (int k = 0; k < 2; k++)
-    //             {
-    //                 mat3[i][j]+=mat1[i][k]*mat2[k][j];
-    //             }
-                
-    //         }
-            
-    // }   
+    }   
     //main logic for addition
     for (int i = 0; i < 2; i++)
     {
